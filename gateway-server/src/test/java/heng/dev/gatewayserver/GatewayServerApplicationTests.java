799e0ece837d5018b2bea3c7366e4330e4d8498d
loemheng840 <limheng7676@gmail.com>
2026-09-17 09:51:36 +0700

    gateway-server

---
+++ gateway-server/src/test/java/heng/dev/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package heng.dev.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ itp-identity-server/src/test/java/heng/dev/itpidentityserver/ItpIdentityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package heng.dev.itpidentityserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItpIdentityServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
